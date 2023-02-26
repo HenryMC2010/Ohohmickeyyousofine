b71c33fdf4eacc8ae36b80c8c492e5066ce8015d
HenryMC2010 <enderboy6664@gmail.com>
2023-02-26 14:41:02 +0700

    oh oh mikey you so fine

---
+++ hah.js
@@ -0,0 +1,40 @@
+var demo=document.getElementById('demo')
+const body=document.querySelector('body')
+var button=document.getElementsById('button')
+const toggle=document.getElementById('toggle')
+function hoithoai(){
+    body.style.background="#131313"
+    body.style.transition="0.5s"
+}
+function hoithoai(){
+    alert ("trong khi dang ngu,...")
+    alert ("toi thays nguoi ban than yeu cua toi dang o cho nao ko ro")
+    alert ("cu the toi chi nho la dang o mot ben")
+    alert ("nhung ben nay rat ki la")
+    alert ("no toan la nhung con tau bay")
+    alert ("toi noi thich cau ay tu khi nao?")
+    alert ("cau ay noi: cau song khoe nhe, nhung...")
+    alert ("vao mot ngay mua dam cuoi, cau se thay toi va mot ai do khac...")
+    alert ("...bao trong nhe")
+    alert ("cau ay roi di")
+    alert ("toi bung tinh day")
+    alert ("toi nhan ra do la giac mo")
+    alert ("mac du la giac mo, nhung toi da roi vai giot nuoc mat")
+    body.style.background="white"
+    body.style.transition="0.5s"
+}
+function dentoi(){
+    body.style.background="black"
+    body.style.transition="0.5s"
+    if(body.style.background="black"){
+        arlert ("ehehe")
+        body.style.background="white"
+    body.style.transition="0.5s"
+    document.getElementById("toggler").innerHTML = "Ehehe";
+    document.getElementById("toggler").style.color="white"
+    }
+    else{
+        body.style.background="white"
+    body.style.transition="0.5s"
+    }
+}
